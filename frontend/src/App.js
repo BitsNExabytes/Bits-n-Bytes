5bdea0ad8715b0d616fcc31042c09012c126b39b
= <sandrock11@hotmail.com>
2021-01-09 16:31:33 -0500

    Added header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+//we must bring in the container component from React Bootstrap to use it
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 const App = ()=> {
   return (
+
+    // empty tags represent fragments
     <>
-   <h1>Chad's web store</h1>
+        <Header/>
+        <main className = 'py-3'>
+          <Container>
+          <h1>Chad's web store</h1>
+          </Container>
+      
+        </main>
+        <Footer/>
+       
     </>
   );
 }
